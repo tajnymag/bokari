351f555a4d0fafb05ce2c663005c081c3eee364e
Marek Lukáš <tajnymag@gmail.com>
2020-11-08 16:36:19 +0100

    Extended contracts controller

--- packages/server/src/controllers/contracts.controller.ts
+++ packages/server/src/controllers/contracts.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get } from 'tsoa';
+import { Controller, Get, Post, Body, Request, Security } from 'tsoa';
 import { Contract } from '@bokari/shared';
+import { ContractWhereUniqueInput } from '@bokari/database';
+import { Forbidden } from '@curveball/http-errors';
 import { db } from '../common/db';
+import { TsoaRequest } from '../middlewares/authentication';
 
 export class ContractsController extends Controller {
 	@Get()
@@ -20,12 +23,28 @@ export class ContractsController extends Controller {
 				},
 				contractComments: {
 					include: {
-						comment: true
+						comment: {
+							include: {
+								author: {
+									include: {
+										person: true
+									}
+								}
+							}
+						}
 					}
 				},
 				contractFiles: {
 					include: {
-						file: true
+						file: {
+							include: {
+								author: {
+									include: {
+										person: true
+									}
+								}
+							}
+						}
 					}
 				},
 				contractLevels: {
@@ -44,8 +63,34 @@ export class ContractsController extends Controller {
 			description: rc.description ?? undefined,
 			startAt: rc.startAt,
 			name: rc.name,
-			comments: [],
-			files: [],
+			comments: rc.contractComments
+				.map((rcc) => rcc.comment)
+				.map((c) => ({
+					id: c.id,
+					content: c.content,
+					createdAt: c.createdAt,
+					updatedAt: c.updatedAt,
+					author: {
+						id: c.author.id,
+						username: c.author.username,
+						name: c.author.person.name
+					}
+				})),
+			files: rc.contractFiles
+				.map((rcf) => rcf.file)
+				.map((f) => ({
+					id: f.id,
+					filename: f.name,
+					url: `/static/${f.hash}`,
+					blurHash: f.blurhash ?? undefined,
+					createdAt: f.createdAt,
+					type: f.type,
+					author: {
+						id: f.author.id,
+						username: f.author.username,
+						name: f.author.person.name
+					}
+				})),
 			levels: [],
 			subcontracts: [],
 			price: {
@@ -62,5 +107,48 @@ export class ContractsController extends Controller {
 		}));
 
 		return contracts;
+	}
+
+	@Security('jwt_access')
+	@Post()
+	public async createContract(@Body() contract: Contract, @Request() request: TsoaRequest) {
+		if (await this.existsContract({ code: contract.code })) {
+			throw new Forbidden('A contract with such code already exists!');
+		}
+
+		const createdContract = await db.contract.create({
+			data: {
+				code: contract.code,
+				name: contract.name,
+				deadlineAt: contract.deadlineAt,
+				client: {
+					connect: {
+						id: contract.client.id
+					}
+				},
+				price: {
+					create: {
+						amount: contract.price.amount,
+						currency: {
+							connect: {
+								isoCode: contract.price.currency
+							}
+						}
+					}
+				},
+				responsibleUser: {
+					connect: {
+						id: contract.responsibleUser.id
+					}
+				},
+				createdById: request.jwt?.user.id
+			}
+		});
+	}
+
+	private async existsContract(query: ContractWhereUniqueInput): Promise<boolean> {
+		const foundId = db.contract.findOne({ where: query, select: { id: true } });
+
+		return foundId !== null;
 	}
 }
